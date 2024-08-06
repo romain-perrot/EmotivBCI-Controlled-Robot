261d20aeba35b4a5b9f71c8f8ca4390bfbbed0f5
Romain Perrot <122283320+romain-perrot@users.noreply.github.com>
2024-08-06 15:37:18 +0100

    Update trainingML.py

--- mind_control/src/trainingML.py
+++ mind_control/src/trainingML.py
@@ -3,7 +3,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import LogisticRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from joblib import dump
 
